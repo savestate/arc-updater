3751ceb00591a61da844ee577f924133c050682f
savestate <verticalfor@gmail.com>
2022-11-06 12:33:39 -0500

    made it better

--- arc-updater.go
+++ arc-updater.go
@@ -18,16 +18,25 @@ import (
 )
 
 var (
+	arcLog       *log.Logger
 	MD5FileName  string = "d3d11.dll.md5sum"
 	dllFileName  string = "d3d11.dll"
 	transportTLS        = &http.Transport{
-		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true}, //no sensitive data is being transmitted, no TLS is "okay"
 	}
 	requestClient = http.Client{
 		Timeout:   5 * time.Second,
 		Transport: transportTLS,
 	}
 )
+
+func init() {
+	logFile, err := os.OpenFile("arcUpdater-latestLog.txt", os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0666)
+	if err != nil {
+		log.Fatal(err)
+	}
+	arcLog = log.New(logFile, "INF: ", log.Ldate|log.Ltime)
+}
 
 func main() {
 	targetSlice := []string{
@@ -38,11 +47,12 @@ func main() {
 	}
 	if request(targetSlice[0]) { // If the site is responding
 		if !checkDllExists() {
-			fmt.Println("Arcdps not found - Installing")
+			arcLog.Println("Arcdps not found - Installing")
 			site := getSiteMD5(targetSlice[3])
 			local := getNewVersion(targetSlice[2])
+			arcLog.Println("Comparing Hashes -> ", site, local)
 			if site == local {
-				fmt.Println("Checksum OK - Installing Arcdps.")
+				arcLog.Println("Checksum OK - Installing Arcdps.")
 				err := os.Rename("./d3d11.dll", "../d3d11.dll")
 				if err != nil {
 					fmt.Printf("%v", err)
@@ -53,7 +63,7 @@ func main() {
 				}
 				return
 			} else {
-				fmt.Println("Checksums do not match - Arcdps will not be Installed.")
+				arcLog.Println("Checksums do not match - Arcdps will not be Installed.")
 				err := os.Remove(dllFileName)
 				if err != nil {
 					fmt.Printf("%v", err)
@@ -68,20 +78,21 @@ func main() {
 		site := getSiteMD5(targetSlice[3])
 		local := getLocalMD5()
 		if site != local { //if the sums are different for local version and site version
+			arcLog.Println("Checksums do not match -> Updating")
 			new := getNewVersion(targetSlice[2])
 			if site == new {
-				fmt.Println("Checksum OK - Updating Arcdps.")
+				arcLog.Println("Downloaded Checksum OK - Updating Arcdps.")
 				err := os.Rename("./d3d11.dll", "../d3d11.dll")
 				if err != nil {
-					fmt.Printf("%v", err)
+					arcLog.Printf("%v", err)
 				}
 				err = os.Remove(MD5FileName)
 				if err != nil {
-					fmt.Printf("%v", err)
+					arcLog.Printf("%v", err)
 				}
 				return
 			} else {
-				fmt.Println("Checksums do not match - Arcdps will not be updated.")
+				arcLog.Println("Checksums do not match - Arcdps will not be updated.")
 				err := os.Remove(dllFileName)
 				if err != nil {
 					fmt.Printf("%v", err)
@@ -93,17 +104,17 @@ func main() {
 				return
 			}
 		} else {
-			fmt.Println("Your Arcdps is already the latest version. Exiting.")
+			arcLog.Println("Arcdps is already up to date. No changes made.")
 			err := os.Remove(MD5FileName)
 			if err != nil {
-				fmt.Printf("%v", err)
+				arcLog.Printf("%v", err)
 			}
 		}
 	} else {
-		fmt.Println("Bad Response from https://www.deltaconnected.com/arcdps/ - Exiting.")
+		arcLog.Println("Bad Response from https://www.deltaconnected.com/arcdps/ - Exiting.")
 	}
-	fmt.Println("Finished - Press Enter to exit.")
-	fmt.Scanln()
+	// fmt.Println("Finished - Press Enter to exit.")
+	// fmt.Scanln()
 }
 
 func request(target string) bool {
@@ -122,24 +133,24 @@ func request(target string) bool {
 func getSiteMD5(md5URL string) string {
 	sumfile, err := os.Create(MD5FileName)
 	if err != nil {
-		panic(err)
+		arcLog.Panic(err)
 	}
 	defer sumfile.Close()
 
 	resp, err := requestClient.Get(md5URL)
 	if err != nil {
-		log.Fatal(err)
+		arcLog.Fatal(err)
 	}
 	defer resp.Body.Close()
 
 	_, err = io.Copy(sumfile, resp.Body)
 	if err != nil {
-		panic(err)
+		arcLog.Panic(err)
 	}
 
 	oFile, err := os.Open(sumfile.Name())
 	if err != nil {
-		panic(err)
+		arcLog.Panic(err)
 	}
 	defer oFile.Close()
 
@@ -161,7 +172,7 @@ func getLocalMD5() string {
 
 	_, err = io.Copy(hash, sumfile)
 	if err != nil {
-		panic(err)
+		arcLog.Panic(err)
 	}
 	return fmt.Sprintf("%x", hash.Sum(nil))
 }
@@ -169,30 +180,30 @@ func getLocalMD5() string {
 func getNewVersion(dllURL string) string {
 	file, err := os.Create(dllFileName)
 	if err != nil {
-		panic(err)
+		arcLog.Panic(err)
 	}
 	defer file.Close()
 	resp, err := requestClient.Get(dllURL)
 	if err != nil {
-		log.Fatal(err)
+		arcLog.Fatal(err)
 	}
 	defer resp.Body.Close()
 
 	_, err = io.Copy(file, resp.Body)
 	if err != nil {
-		panic(err)
+		arcLog.Panic(err)
 	}
 
 	//verify downloaded sum matches the posted sum
 	hash := md5.New()
 	nFile, err := os.Open(dllFileName)
 	if err != nil {
-		panic(err)
+		arcLog.Panic(err)
 	}
 	defer nFile.Close()
 	_, err = io.Copy(hash, nFile)
 	if err != nil {
-		panic(err)
+		arcLog.Panic(err)
 	}
 	return fmt.Sprintf("%x", hash.Sum(nil))
 }
@@ -200,12 +211,12 @@ func getNewVersion(dllURL string) string {
 func checkDllExists() bool {
 	if _, err := os.Stat("../d3d11.dll"); err != nil {
 		if errors.Is(err, fs.ErrNotExist) {
-			fmt.Println("d3d11.dll Does Not Exist.")
+			arcLog.Println("d3d11.dll Does Not Exist.")
 			return false
 		} else {
 			panic(err)
 		}
 	}
-	fmt.Println("d3d11.dll Exists.")
+	arcLog.Println("d3d11.dll Exists.")
 	return true
 }
